90b9ae10e4c4db2996894c2e0ffc64c503b7f257
louis watson <luigiwatson1@gmail.com>
2017-03-20 17:53:34 +0000

    setuppuisghgrsgrsg

--- www/scripts/scripts.js
+++ www/scripts/scripts.js
@@ -1,3 +1,60 @@
+document.addEventListener("deviceready", setupPush, false);
+function setupPush() {
+var push = PushNotification.init({
+"android": {
+"senderID": "436479437581"
+},
+"ios": {
+"sound": true,
+"alert": true,
+"badge": true,
+"categories": {
+"invite": {
+"yes": {
+"callback": "app.accept", "title": "Accept", "foreground": true, "destructive": false
+},
+"no": {
+"callback": "app.reject", "title": "Reject", "foreground": true, "destructive": false
+},
+"maybe": {
+"callback": "app.maybe", "title": "Maybe", "foreground": true, "destructive": false
+}
+},
+"delete": {
+"yes": {
+"callback": "app.doDelete", "title": "Delete", "foreground": true, "destructive": true
+},
+"no": {
+"callback": "app.cancel", "title": "Cancel", "foreground": true, "destructive": false
+}
+}
+}
+},
+"windows": {}
+});
+push.on('registration', function(data) {
+console.log("registration event: " + data.registrationId);
+var oldRegId = localStorage.getItem('registrationId');
+if (oldRegId !== data.registrationId) {
+// Save new registration ID
+localStorage.setItem('registrationId', data.registrationId);
+// Post registrationId to your app server as the value has changed
+}
+});
+push.on('error', function(e) {
+console.log("push error = " + e.message);
+});
+push.on('notification', function(data) {
+console.log('notification event');
+navigator.notification.alert(
+data.message, // message
+null, // callback
+data.title, // title
+'Ok' // buttonName
+);
+});
+
+
 $(document).ready(function() {
 	console.log("hello");
 
